2a0fa77a913eb848efa44c173bb5cbee5665976e
RayyKh <rayykhal894@gmail.com>
2026-03-18 01:12:59 +0100

    fix: force timezone to Africa/Tunis in backend and DB connection

--- backend/src/main/java/com/barbershop/BarbershopApplication.java
+++ backend/src/main/java/com/barbershop/BarbershopApplication.java
@@ -2,9 +2,17 @@ package com.barbershop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class BarbershopApplication {
+
+	@PostConstruct
+	public void init() {
+		// Setting Spring Boot SetTimeZone
+		TimeZone.setDefault(TimeZone.getTimeZone("Africa/Tunis"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BarbershopApplication.class, args);
